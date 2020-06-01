1e966ff2c3744aba89e97b5cd099b30c074a423f
AnCIity <city@nanzc.com>
2020-06-01 15:36:27 +0800

    预览

---
+++ precache-manifest.0703b783aaa79f39bafd7803a2bcd9c5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65efccc0a9712ddc8e5e7f53810c791f",
+    "url": "/ZhiHu-Daily/index.html"
+  },
+  {
+    "revision": "0b2e0d9a8a1101d94060",
+    "url": "/ZhiHu-Daily/static/css/4.3c1f251b.chunk.css"
+  },
+  {
+    "revision": "8d6e710a0be80227071c",
+    "url": "/ZhiHu-Daily/static/css/5.56907da8.chunk.css"
+  },
+  {
+    "revision": "b233851bce8c2894dfc4",
+    "url": "/ZhiHu-Daily/static/css/6.7122b174.chunk.css"
+  },
+  {
+    "revision": "2f21ce7a5e5f94a460c1",
+    "url": "/ZhiHu-Daily/static/css/7.b95de51a.chunk.css"
+  },
+  {
+    "revision": "80562e9b9d5f3075b8f6",
+    "url": "/ZhiHu-Daily/static/css/8.7cb0c5a9.chunk.css"
+  },
+  {
+    "revision": "19d032a6e4d55d040178",
+    "url": "/ZhiHu-Daily/static/css/main.bd9bf933.chunk.css"
+  },
+  {
+    "revision": "16103a20295d39e048f5",
+    "url": "/ZhiHu-Daily/static/js/0.3ebac570.chunk.js"
+  },
+  {
+    "revision": "b94e17593a4bde3f153f",
+    "url": "/ZhiHu-Daily/static/js/3.86a67cf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ZhiHu-Daily/static/js/3.86a67cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2e0d9a8a1101d94060",
+    "url": "/ZhiHu-Daily/static/js/4.9f3b570e.chunk.js"
+  },
+  {
+    "revision": "8d6e710a0be80227071c",
+    "url": "/ZhiHu-Daily/static/js/5.ae36b486.chunk.js"
+  },
+  {
+    "revision": "b233851bce8c2894dfc4",
+    "url": "/ZhiHu-Daily/static/js/6.11aa6f80.chunk.js"
+  },
+  {
+    "revision": "2f21ce7a5e5f94a460c1",
+    "url": "/ZhiHu-Daily/static/js/7.6fb8a3b2.chunk.js"
+  },
+  {
+    "revision": "80562e9b9d5f3075b8f6",
+    "url": "/ZhiHu-Daily/static/js/8.d23aa652.chunk.js"
+  },
+  {
+    "revision": "19d032a6e4d55d040178",
+    "url": "/ZhiHu-Daily/static/js/main.c6507177.chunk.js"
+  },
+  {
+    "revision": "01713a4ce83d0b735c4b",
+    "url": "/ZhiHu-Daily/static/js/runtime-main.5e08f8c5.js"
+  }
+]);
